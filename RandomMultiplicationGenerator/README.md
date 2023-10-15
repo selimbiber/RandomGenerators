b1fdbbc1b57bc485c79d546636d4386dd079fb1e
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-10-15 13:14:18 +0300

    Finish the project of Random Multiplication Generator

---
+++ RandomMultiplicationGenerator/README.md
@@ -0,0 +1,16 @@
+# [Simple Note Taking Application | Basit Not Alma Uygulaması](https://www.100jsprojects.com/project/multiplication-app/)
+
+## Project Description | Proje Açıklaması
+
+100jsproject.com kaynaklı javascript projeleri yapmak bence kolay ve keyifli oluyor. Mikro ölçektede olsa bu basit projeleri yapmaktaki asıl amacım, javascript'i daha efektif şekilde kullanabilmek, okunaklı ve sürdürülebilir kod yazabilme becerilerimi geliştirmektir.
+
+I find it easy and fun to make javascript projects from 100jsproject.com. My main goal in making these simple projects, even on a micro scale, is to use javascript more effectively and improve my skills in writing legible and maintainable code.
+
+### [Preview Link | Önizleme Bağlantısı](https://htmlpreview.github.io/?https://github.com/selimbiber/PureJavaScriptProjects/blob/main/MultiplicationApplication/index.html)
+
+#### [Profile Links | Profil Bağlantıları](https://github.com/selimbiber/)
+
+- Portfolio Website - [selimbiber.dev](https://www.selimbiber.dev)
+- Frontend Mentor - [@selimbiber](https://www.frontendmentor.io/profile/selimbiber)
+- LinkedIn - [Selim Biber](https://linkedin.com/in/selim-biber-406550214)
+- Twitter - [@selimbbr](https://www.twitter.com/selimbbr)
---
+++ RandomMultiplicationGenerator/main.js
@@ -0,0 +1,45 @@
+let currentScore = 0;
+let numX = 5; //? DEFAULT VALUE
+let numY = 3; //? DEFAULT VALUE
+
+let correctAnswer = numX * numY;
+
+const GenerateNumbers = {
+    generateNumX: () => {
+        return Math.floor( (Math.random() * 10) + 1);
+    },
+    generateNumY: () => {
+        return Math.floor( (Math.random() * 10) + 1);
+    }
+}
+
+//? Event Listeners
+
+document.querySelector('.submit-btn').onclick = () => {
+    correctAnswer == document.querySelector('#user-answer').value ? updateAllValues('+') : updateAllValues('-');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    currentScore = JSON.parse( window.localStorage.getItem('currentScore') );
+    document.querySelector('.current-score').textContent = currentScore;
+});
+
+//? Helper Functions
+
+function generateNewMultiplicationQuestion() {
+    numX = GenerateNumbers.generateNumX();
+    numY = GenerateNumbers.generateNumY();
+
+    document.querySelector('.random-question').textContent = `What is ${numX} multiply by ${numY}?`;
+
+    return correctAnswer = numX * numY;
+}
+generateNewMultiplicationQuestion();
+
+function updateAllValues(operator) {
+    operator === '-' ? --currentScore : ++currentScore;
+    document.querySelector('.current-score').textContent = currentScore;
+    generateNewMultiplicationQuestion();
+    window.localStorage.setItem( 'currentScore', JSON.stringify(currentScore) );
+    document.querySelector('#user-answer').value = '';
+}
